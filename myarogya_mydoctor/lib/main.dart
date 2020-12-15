7b5b69af2c254f870af939d906b0874c8b3c9d62
Harun <harun.zigainfotech@gmail.com>
2020-12-15 12:31:17 +0530

    dfd

--- myarogya_mydoctor/lib/main.dart
+++ myarogya_mydoctor/lib/main.dart
@@ -29,17 +29,19 @@ class MyApp extends StatelessWidget {
         theme: new ThemeData(
           primaryColor: Colors.redAccent,
           accentColor: Colors.redAccent,
-          pageTransitionsTheme: PageTransitionsTheme(builders: {TargetPlatform.android: CupertinoPageTransitionsBuilder(),}),
+          pageTransitionsTheme: PageTransitionsTheme(builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+          }),
         ),
         debugShowCheckedModeBanner: false,
         // home: MyIncomePage("Income Settings","+919444773937"),
-      // home: Bookdetailed("Revathi"),
-      //   home: SplashScreen()
+        // home: Bookdetailed("Revathi"),
+        //   home: SplashScreen()
 //        home: AddHospital(),
 //      home: PatientNewDashboard("0BPnrwIHQDR0ABiYiaIFc1IaSX83","+917810081326")
-         home: DoctorNewDashboard("pfEKUrxcZWa5UFTYNwqMimJbVo93","+918610905528")
+        home:
+            DoctorNewDashboard("2T2HZP3UWJTfMjzr4TbT7hhE9qz1", "+919444773937")
 //      home: HospitalDashboard("2T2HZP3UWJTfMjzr4TbT7hhE9qz1","+919444773937")
-    );
+        );
   }
 }
-
